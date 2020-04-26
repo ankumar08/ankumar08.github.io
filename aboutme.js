3f9fbaec23c1da73a6da282baa8083fba5b421ad
ankumar08 <63973563+ankumar08@users.noreply.github.com>
2020-04-26 11:29:12 -0400

    Update aboutme.js

--- aboutme.js
+++ aboutme.js
@@ -1,6 +1,6 @@
 //Create Contact Me button
 //When clicked, button expands to show email address, phone number, city, state
 function seeMore(){
-  var information = document.getElementById("contact");
+  var information = document.getElementById("contactinfo");
   information.innerHTML = "kumaranika96@gmail.com; 415-497-7841; Boston,MA"
 }
